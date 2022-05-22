18710f539e07ed2f07231e08b2e71c5b945532a3
Nayan K. Mevada <nkmevada1997@gmail.com>
2022-05-22 11:58:03 +0530

    - Added Login For Remember Me !

--- Ecommerce.Models/Authentication/LoginModel.cs
+++ Ecommerce.Models/Authentication/LoginModel.cs
@@ -5,12 +5,14 @@ namespace Ecommerce.Models.Authentication
     public class LoginModel
     {
         [Required]
-        [Display(Name ="Email")]
+        [Display(Name = "Email")]
         public string Email { get; set; } = string.Empty;
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name ="Password")]
+        [Display(Name = "Password")]
         public string Password { get; set; } = string.Empty;
+
+        public bool RememberMe { get; set; }
     }
 }
